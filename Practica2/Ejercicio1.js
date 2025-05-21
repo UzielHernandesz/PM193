02e1848ea0ee0a6a5dff50ef971067321ae56424
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2025-05-20 20:07:39 -0600

    EJERCICIO 1 PRACTICA 2

---
+++ Practica2/Ejercicio1.js
@@ -0,0 +1,14 @@
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion: {
+        ciudad: "Qro",
+        pais: "Mexico",
+    }
+};
+// imprimir destructuacion aqui
+const { nombre, edad, direccion: { ciudad } } = persona;
+// imprimir el mensaje aqui
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años.`); // "Hola, me llamo Ivan Isay y tengo 37 años."
+
+
