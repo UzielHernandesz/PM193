ffff058ac5590be7c53102df9c27da5038741eb9
UzielHernandesz <146143112+UzielHernandesz@users.noreply.github.com>
2025-05-23 14:30:31 -0600

    EJERCICIO A PRACTICA 3

---
+++ Practica3/Utils.js
@@ -0,0 +1,7 @@
+// Definir la función restar
+function restar(a, b) {
+    return a - b;
+}
+
+// Exportar la función
+module.exports = restar;
---
+++ Practica3/main.js
@@ -0,0 +1,9 @@
+// Importar la función restar desde utils.js
+const restar = require('./Utils');
+
+// Ejecutar varias pruebas
+console.log(restar(10, 3)); // 7
+console.log(restar(20, 12)); // 8
+console.log(restar(23, 2));  // 21
+console.log(restar(150, 45)); // 105
+console.log(restar(50, 20)); // 30
